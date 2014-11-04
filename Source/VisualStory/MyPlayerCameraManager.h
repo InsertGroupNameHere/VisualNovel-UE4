63e366408b91fb28444d6440f8a4c9b86420983d
Ademola Afolabi <kingsminos@gmail.com>
2014-11-04 22:28:16 +0000

    Zack Complex Build #1
    This is the scene where the main character will be staying in.

---
+++ Source/VisualStory/MyPlayerCameraManager.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VISUALSTORY_API AMyPlayerCameraManager : public APlayerController
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
